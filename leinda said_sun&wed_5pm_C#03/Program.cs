341d194b272aae9dd5e6431391354a7457aa59bb
Leinda Demy <demyleinda@gmail.com>
2024-08-26 17:46:36 +0300

    Initial commit

---
+++ leinda said_sun&wed_5pm_C#03/Program.cs
@@ -0,0 +1,265 @@
+﻿namespace leinda_said_sun_wed_5pm_C_03 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region question_1
+            //Console.WriteLine("please enter a number");
+            //if (int.TryParse(Console.ReadLine(), out int number))
+            //{
+            //    if (number % 3 == 0 && number % 4 == 0)
+            //    {
+            //        Console.WriteLine("Yes");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("No");
+            //    }
+            //}
+
+            #endregion
+
+            #region question_2
+            //Console.WriteLine("please enter a number");
+            //if (int.TryParse(Console.ReadLine(), out int number))
+            //{
+            //    if (number < 0)
+            //    {
+            //        Console.WriteLine("Negative");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Positive");
+            //    }
+            //}
+            #endregion
+
+            #region question_3
+            //int max = int.MinValue;
+            //int min = int.MaxValue;
+            //int [] numbers = new int[3];
+            //Console.WriteLine("please enter first number");
+            //if (int.TryParse(Console.ReadLine(), out int number_1))
+            //{
+            //    Console.WriteLine("please enter second number");
+            //    if (int.TryParse(Console.ReadLine(), out int number_2))
+            //    {
+            //        Console.WriteLine("please enter last number");
+            //        if (int.TryParse(Console.ReadLine(), out int number_3))
+            //        {
+            //            numbers[0] = number_1;
+            //            numbers[1] = number_2;
+            //            numbers[2] = number_3;  
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] > max)
+            //    {
+            //        max = numbers[i];
+            //    }
+            //    if (numbers[i] < min)
+
+            //    {
+            //        min = numbers[i];
+            //    }
+            //}
+            //Console.WriteLine("max =" + max);
+            //Console.WriteLine("min =" + min);
+            #endregion
+
+            #region question_4
+            //Console.WriteLine("insert an integer number then check If a number is even or odd.");
+            //if (int.TryParse(Console.ReadLine(), out int number))
+
+            //{
+            //    if (number % 2 == 0)
+            //    {
+            //        Console.WriteLine("Even");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Odd");
+            //    }
+            //}
+            #endregion
+
+            #region question_5
+            //Console.WriteLine("Enter a character : ");
+            //if (char.TryParse(Console.ReadLine(), out char character))
+            //{
+            //    if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+            //  {
+            //       Console.WriteLine("Vowel");
+            //    }
+            //   else
+            //   {
+            //       Console.WriteLine("Consonant");
+            //  }
+            //}
+            #endregion
+
+            #region question_6
+            //Console.WriteLine("Enter a number : ");
+            //if (int.TryParse(Console.ReadLine(), out int number))
+            //{
+            //    for (int i = 1; i <= number; i++)
+            //    {
+            //        Console.Write(i + ", ");
+            //    }
+            //}
+            #endregion
+
+            #region question_7
+            //Console.WriteLine("Enter a number : ");
+            //if (int.TryParse(Console.ReadLine(), out int number))
+            //{
+            //    for (int i = 1; i <= 12; i++)
+            //    {
+            //        Console.WriteLine( number + " * " + i + " = " + number * i );
+            //    }
+            //}
+            #endregion
+
+            #region question_8
+            //Console.WriteLine("Enter a number : ");
+            //if (int.TryParse(Console.ReadLine(), out int value))
+            //{
+            //    for (int i = 1; i <= value; i++)
+            //    {
+            //        if (i % 2 == 0)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+            //}
+            #endregion
+
+            #region question_9
+            //Console.WriteLine("Enter two numbers : ");
+            //if (int.TryParse(Console.ReadLine(), out int number1) &&
+            //    int.TryParse(Console.ReadLine(), out int number2))
+            //{
+            //    Console.WriteLine(Math.Pow(number1, number2));
+            //}
+            #endregion
+
+            #region question_10
+            //Console.WriteLine("Enter marks of five subjects : ");
+            //int total = 0;
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    if (int.TryParse(Console.ReadLine(), out int number))
+            //    {
+            //        total += number;
+            //    }
+            //}
+            //Console.WriteLine("Total = " + total);
+            //Console.WriteLine("Average = " + total / 5);
+            //Console.WriteLine("Percentage = " + total * 100 / 500);
+
+            #region question_11
+
+            #endregion
+
+            #region question_12
+            // Write a program to create a Simple Calculator.
+
+            //Console.WriteLine("Enter two numbers : ");
+            //if (double.TryParse(Console.ReadLine(), out double number1) &&
+            //    double.TryParse(Console.ReadLine(), out double number2))
+            //{
+            //    Console.WriteLine("Addition = " + (number1 + number2));
+            //    Console.WriteLine("Subtraction = " + (number1 - number2));
+            //    Console.WriteLine("Multiplication = " + (number1 * number2));
+            //    Console.WriteLine("Division = " + (number1 / number2));
+            //}
+
+
+            #endregion
+
+            #region question_13
+            //Console.WriteLine("Enter a string : ");
+            //string name = Console.ReadLine();
+            //for (int i = name.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(name[i]);
+            //}
+            #endregion
+
+            #region question_14
+            //Console.WriteLine("Enter an integer : ");
+            //if (int.TryParse(Console.ReadLine(), out int number))
+            //{
+            //    string value = number.ToString();
+            //    for (int i = int.Parse(value); i >= 0; i--)
+            //    {
+            //        Console.Write(i);
+            //    }
+            //}
+            #endregion
+
+            #region question_15
+
+            #endregion
+
+            #region questtion_16
+            // Write a program in C# Sharp to convert
+            // a decimal number into binary without using an array.
+
+            //Console.WriteLine("Enter a number : ");
+            //if (int.TryParse(Console.ReadLine(), out int number))
+            //{
+            //    Console.WriteLine(Convert.ToString(number, 2));
+            //}
+            #endregion
+
+            #region question_17
+            //Console.WriteLine("Enter three points : ");
+            //if (double.TryParse(Console.ReadLine(), out double x1) &&
+            //    double.TryParse(Console.ReadLine(), out double y1) &&
+            //    double.TryParse(Console.ReadLine(), out double x2) &&
+            //    double.TryParse(Console.ReadLine(), out double y2) &&
+            //    double.TryParse(Console.ReadLine(), out double x3) &&
+            //    double.TryParse(Console.ReadLine(), out double y3))
+            //{
+            //    if ((x1 - x2) * (y2 - y3) == (x2 - x3) * (y1 - y2))
+            //    {
+            //        Console.WriteLine("True");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("False");
+            //    }
+            //}
+            #endregion
+
+            #region question_18
+            //Console.WriteLine("Enter the time taken for the task : ");
+            //if (int.TryParse(Console.ReadLine(), out int time))
+            //{
+            //    if (time <= 3)
+            //    {
+            //        Console.WriteLine("Highly efficient");
+            //    }
+            //    else if (time > 3 && time <= 4)
+            //    {
+            //        Console.WriteLine("Instructed to increase their speed");
+            //    }
+            //    else if (time > 4 && time <= 5)
+            //    {
+            //        Console.WriteLine("Provided with training to enhance their speed");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Required to leave the company");
+            //    }
+            //}
+            #endregion
+
+
+        }
+    }
+}
